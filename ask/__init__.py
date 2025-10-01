76024cc1ba00e8acb761f7b978cddf9e63a4515e
var <github@umdoze.com>
2025-10-01 09:27:11 -0700

    refactor: Remove unused cache store imports for cleaner code

--- ask/__init__.py
+++ ask/__init__.py
@@ -3,21 +3,11 @@ Top-level module exposing the single public class AgentASK.
 """
 
 from ask.core.agent import AgentASK
-from ask.core.cache import (
-    CacheASK,
-    CacheStoreJson,
-    CacheStoreMemory,
-    CacheStoreSQLite,
-    CacheStoreYaml,
-)
+from ask.core.cache import CacheASK
 from ask.core.context import ContextASK
 
 __all__ = [
     "AgentASK",
     "ContextASK",
     "CacheASK",
-    "CacheStoreSQLite",
-    "CacheStoreYaml",
-    "CacheStoreMemory",
-    "CacheStoreJson",
 ]
